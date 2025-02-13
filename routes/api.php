ebab4f8a4c266aad957aae039bf124220e0c937c
Reyshellby <reyshellby@gmail.com>
2025-02-13 14:13:26 +0700

    .....

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\CategoriesController;
 use App\Http\Controllers\Api\WastesController;
 
@@ -15,10 +16,15 @@ use App\Http\Controllers\Api\WastesController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/update/{id}', [AuthController::class, 'update']);
+    Route::apiResource('category',CategoriesController::class);
+    Route::apiResource('wastes',WastesController::class);
 });
-
-Route::apiResource('category',CategoriesController::class);
-Route::apiResource('wastes',WastesController::class);
